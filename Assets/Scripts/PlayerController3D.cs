c340843dec76925fd7c298a58227c8d3eeed7d99
Paulagantes1010 <gantesgomezpaula@gmail.com>
2024-11-14 13:22:40 +0000

    arreglar sonido

---
+++ Assets/Scripts/PlayerController3D.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController3D : MonoBehaviour
+{
+    public float speed = 4f; // velocidad
+    public float speedJump = 15f; //velocidad de salto
+    private Rigidbody playerb; // rigidbody
+    private Vector3 moveInput; // el input en 3D
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal"); //movimiento horizontal
+        float moveZ = Input.GetAxisRaw("Vertical"); //movimiento vertical
+        moveInput = new Vector3( moveX, 0, moveZ); //asignandole donde va cada movimiento
+        if (Input.GetKey("space") && playerb.velocity.y == 0){
+            playerb.AddForce(transform.up * speedJump);
+            Debug.Log("Bien hecho");// para la consola
+        } // cuando apretas el espacio y la velocidad hacia arriba es cero, añadimos una fuerza hacia arriba multiplicado por la velocidad de salto, esto nos permite saltar C:
+    }
+    private void FixedUpdate(){
+        playerb.MovePosition(playerb.position + moveInput.normalized * speed * Time.fixedDeltaTime);
+        // moviminto fluido
+    }
+}
+
